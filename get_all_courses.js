1ab50085d1e362e51ae720ec7c5c20d2671b4c74
Sidhaant Anand <=>
2020-03-29 13:33:30 -0400

    Adding a field of url to /getAllCourses

--- get_all_courses.js
+++ get_all_courses.js
@@ -4,6 +4,9 @@ module.exports = function(req,res) {
 	
 	require('./make_connection.js').then(function([collection,client]) {
 		require('./return_all_courses.js')([collection,client]).then(function(courses) {
+			for(course of courses) {
+				course.url = course._id;
+			}
 			res.send(courses);
 		});
 	});

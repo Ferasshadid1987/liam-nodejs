39e49f745d459628bafa1c28377b6f039a19c26c
Sidhaant Anand <=>
2020-03-29 10:16:40 -0400

    getEverything fix

--- get_everything.js
+++ get_everything.js
@@ -1,8 +1,12 @@
 module.exports = function(req,res) {
 	require('./make_connection.js').then(function([collection,client]) {
-		require('./get_all_documents.js')([collection,client]).then(function(documents) {
+		var username = req.session.currUser;
+		require('./get_document_by_username.js')(username).then(function(doc) {
 			require('./return_all_courses.js')([collection,client]).then(function(courses) {
-				res.send([documents,courses]);
+				res.send({
+					user: doc,
+					courses: courses
+				});
 			});
 		});
 	});
